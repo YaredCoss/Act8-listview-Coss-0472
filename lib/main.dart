f437a8e6299607a4253c7b01bc58c71e8290dace
a.23308051280472 <a.23308051280472@cbtis128.edu.mx>
2026-02-12 20:09:12 +0000

    2 imagenes

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,114 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(AppFlashcards());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AppFlashcards extends StatelessWidget {
+  const AppFlashcards({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: "Flashcards",
+      home: Mazos(),
     );
   }
-}
+} // fin clase AppFlashcards
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Mazos extends StatelessWidget {
+  const Mazos({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          title: const Text(
+    'Flashcards Coss 6J',
+    style: TextStyle(color: Colors.white),
+  ),
+  centerTitle: true,
+  backgroundColor: Colors.blue,
+  leading: const Icon(
+    Icons.camera_alt,
+    color: Color.fromARGB(255, 131, 49, 49),
+  ),
+  actions: [
+    IconButton(
+      icon: const Icon(Icons.favorite, color: Color.fromARGB(255, 150, 7, 7)),
+      onPressed: () {},
+    ),
+    IconButton(
+      icon: const Icon(Icons.edit, color: Color.fromARGB(255, 0, 0, 0)),
+      onPressed: () {},
+    ),
+  ],
+),
+
+body: ListView(
+  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+  children: [
+    _crearElemento(
+      context,
+      icon: Icons.person,
+      color: Colors.amber,
+      titulo: 'Mi perfil ',
+      subtitulo: 'Gestiona tu perfil',
+    ),
+    _crearElemento(
+      context,
+      icon: Icons.deck,
+      color: Colors.blue,
+      titulo: 'Mazos',
+      subtitulo: 'Revisa tus mazos',
+    ),
+    _crearElemento(
+      context,
+      icon: Icons.create,
+      color: Colors.redAccent,
+      titulo: 'Crear mazo',
+      subtitulo: 'Crea tu mazo',
+    ),
+    _crearElemento(
+      context,
+      icon: Icons.settings,
+      color: Colors.teal,
+      titulo: 'Ajustes',
+      subtitulo: 'Personaliza tu experiencia',
+    ),
+  ],
+),
+
     );
   }
+} // fin clase Mazos
+
+Widget _crearElemento(BuildContext context, {
+  required IconData icon, 
+  required Color color, 
+  required String titulo, 
+  required String subtitulo
+}) {
+  return Card( // Usamos Card para darle relieve y que sea más atractivo
+    elevation: 2,
+    margin: const EdgeInsets.symmetric(vertical: 8),
+    child: ListTile(
+      leading: CircleAvatar(
+        backgroundColor: color.withOpacity(0.2),
+        child: Icon(icon, color: color),
+      ),
+      title: Text(titulo, style: const TextStyle(fontWeight: FontWeight.bold)),
+      subtitle: Text(subtitulo),
+      trailing: const Icon(Icons.chevron_right),
+      onTap: () {
+        // Esto hace que el elemento sea funcional mostrando un aviso
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Abriendo: $titulo'),
+            duration: const Duration(seconds: 1),
+            backgroundColor: color,
+          ),
+        );
+      },
+    ),
+  );
 }
